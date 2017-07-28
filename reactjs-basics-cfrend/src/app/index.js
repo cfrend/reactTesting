62a7b28a62413c5c6ce2715be06d8fa10d6bcb37
[Chauncey Frend] <[cfrend@iu.edu]>
2017-07-27 23:45:21 -0400

    going to bed

--- reactjs-basics-cfrend/src/app/index.js
+++ reactjs-basics-cfrend/src/app/index.js
@@ -6,9 +6,22 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+constructor(){
+      super();
+      this.state = {
+            homeLink: "Home"
+      };
+}
 //example function
 onGreet(){
       alert("Hello!");
+}
+
+onChangeLinkName(newName){
+      //trigger a rerendering of the UI
+      this.setState({
+            homeLink: newName
+      });
 }
 
   render() {
@@ -16,13 +29,19 @@ onGreet(){
              <div className="container">
                   <div className="row">
                         <div className="col-xs-10 col-xs-offset-1">
-                              <Header homeLink="Home"/>
+                              <Header homeLink={this.state.homeLink}/>
                         </div>
                   </div>
                   <div className="row">
                         <div className="col-xs-10 col-xs-offset-1">
                               {/*JSX Comment syntax & calling the Home component and passing some prop values including a function as a prop greet*/}
-                              <Home name={"Chauncey"} initialAge={32} greet={this.onGreet}/>
+                              <Home
+                                    name={"Chauncey"}
+                                    initialAge={32}
+                                    greet={this.onGreet}
+                                    changeLink={this.onChangeLinkName.bind(this)}
+                                    initialLinkName={this.state.homeLink}
+                              />
                         </div>
                   </div>
              </div>
